a48d6b0d478caccf466fd4a5bb515675d669d1a9
Vitor Dino <vitor@kunstdesign.com.br>
2018-08-22 21:53:04 -0300

    components: add Container

---
+++ src/components/Container.js
@@ -0,0 +1,20 @@
+import styled, {css} from 'styled-components'
+import {above} from '../utils/responsive'
+import {containerWidth} from '../constants'
+
+const Container = styled.div`
+	margin-left: auto;
+	margin-right: auto;
+	display: block;
+	width: 100%;
+	max-width: ${containerWidth};
+	padding: 0 1rem;
+	${above.md`
+		padding: 0;
+		width: 95%;
+	`}
+	${above.xg`width: 100%;`}
+	${p => p.relative && `position: relative;`}
+`
+
+export default Container
