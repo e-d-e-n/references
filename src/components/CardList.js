c251ee634ab1d77f43fadecee996d2d741951733
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-08-23 20:15:51 -0300

    lint

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import {Link as UnstyledLink} from 'gatsby'
 import {colors, categories} from '../constants'
