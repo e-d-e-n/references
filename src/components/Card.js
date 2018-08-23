3047b10227cac6f4698c3c26eaec17813eb07f04
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-08-23 04:17:11 -0300

    print

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,51 +1,76 @@
 import React from 'react'
 import styled from 'styled-components'
 import {colors} from '../constants'
+import {screen, print} from '../utils/responsive'
 import {Heading, Paragraph} from './Text'
 import Pills from './Pills'
-
-const ImageWrapper = styled.div`
-	background: ${colors.black};
-	display: flex;
-	align-items: center;
-	height: 16em;
-	margin: 0.5rem 0;
-	overflow: hidden;
-`
-
-const Image = styled.img`
-	width:  100%;
-	object-position: center;
-	object-fit: cover;
-	display: block;
-	color: ${colors.brand};
-`
-
-const Description = styled(Paragraph)`
-	max-height: 3.5em;
-	overflow: hidden;
-`
 
 const Wrapper = styled.a`
 	display: block;
 	margin: 4rem 0;
 	text-decoration: none;
-	color: currentColor;
 	page-break-inside: avoid;
-	:hover, :focus, :active {
+	color: currentColor;
+	:hover {
 		color: ${colors.brand};
-		${Image} {
-			opacity: 0.22;
-			filter: grayscale(1);
-			mix-blend-mode: overlay;
+	}
+	${print`
+		&:after{
+			display: table;
+			content: ' ';
+			clear: both;
 		}
-		${ImageWrapper} {
+	`}
+`
+
+const ImageWrapper = styled.div`
+	overflow: hidden;
+	display: block;
+	${screen`
+		margin: 0.5rem 0;
+		height: 16em;
+		display: flex;
+		align-items: center;
+		background: ${colors.black};
+		${Wrapper}:hover & {
 			background: ${colors.brand};
 		}
-		${Description} {
+	`}
+	${print`
+		float: left;
+		width: 41.666%;
+		padding-right: 2rem;
+	`}
+`
+
+const Image = styled.img`
+	width: 100%;
+	object-position: center;
+	object-fit: cover;
+	display: block;
+	${Wrapper}:hover & {
+		opacity: 0.22;
+		filter: grayscale(1);
+		mix-blend-mode: overlay;
+	}
+`
+
+const TextWrapper = styled.div`
+	${print`
+		overflow: hidden;
+		float: left;
+		width: 58.333%;
+	`}
+`
+
+const Description = styled(Paragraph)`
+	${screen`
+		overflow: hidden;
+		max-height: 3.5em;
+		${Wrapper} & {
 			height: auto;
 		}
-	}
+	`}
 `
 
 const Card = ({
@@ -63,12 +88,14 @@ const Card = ({
 	thumbnails: {small = {}, large = {}, full = {}},
 }) => (
 	<Wrapper href={url && url} target='_blank'>
-		{image && <ImageWrapper><Image src={image}/></ImageWrapper>}
-		{name && <Heading size={3}>{name}</Heading>}
-		<Pills icon='log-in' entries={input}/>
-		<Pills icon='zap' entries={transform}/>
-		<Pills icon='log-out' entries={output}/>
-		{(description || null) && <Description>{description}</Description>}
+		<ImageWrapper><Image src={image}/></ImageWrapper>
+		<TextWrapper>
+			{name && <Heading size={3}>{name}</Heading>}
+			<Pills icon='log-in' entries={input}/>
+			<Pills icon='zap' entries={transform}/>
+			<Pills icon='log-out' entries={output}/>
+			{(description || null) && <Description>{description}</Description>}
+		</TextWrapper>
 	</Wrapper>
 )
 
